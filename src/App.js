02c3c46d58edfdb8c448c61880044fe5c29998bf
Radoslav Demirov <rdemirov@gmail.com>
2017-08-20 22:38:10 +0300

    feat : Get books from API call.Adjust display of components

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     showSearchPage: false,
-    shelves:[],
-    books:[]
+    shelves: [
+      { title: 'Currently Reading', name: 'currentlyReading' },
+      { title: 'Read', name: 'read' },
+      { title: 'Want To Read', name: 'wantToRead' }
+    ],
+    books: []
+  }
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) =>
+        this.setState({ books })
+      );
   }
 
   render() {
     return (
       <div className="app">
-          <SearchBooks showSearchPage={this.state.showSearchPage}/>
-          <MyReads shelves={this.state.shelves} books={this.state.books} />
+        <SearchBooks showSearchPage={this.state.showSearchPage} />
+        <MyReads shelves={this.state.shelves} books={this.state.books} />
       </div>
     )
   }
