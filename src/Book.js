1f64be12860af89e5575f85fde1a7be7d653592b
Radoslav Demirov <rdemirov@gmail.com>
2017-08-24 19:24:21 +0300

    fix : Issue with selection of the 'Currently Reading' shelf

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,5 @@
 import React from 'react';
 class Book extends React.Component {
-  state={
-    value:[]
-  }
     render () {
       let book=this.props.book;
         return (
@@ -10,13 +7,13 @@ class Book extends React.Component {
             <div className="book-top">
               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.thumbnail})` }}></div>
               <div className="book-shelf-changer">
-                <select value={this.state.value} onChange={(event)=>
+                <select defaultValue={book.shelf} onChange={(event)=>
                   {
                   let newShelf=event.target.value;
                   this.props.onMoveToShelf({bookId:book.id,newShelf:newShelf})
                   }
                   } >
-                  <option value="none" >Move to...</option>
+                  <option value="none" disabled>Move to...</option>
                   <option value="currentlyReading" >Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
                   <option value="read">Read</option>
